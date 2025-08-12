345937ffe14251d2d799183e7a78b9d4aa79cc55
missrabbit0505-del <missrabbit0505@yahoo.com.tw>
2025-08-12 15:37:03 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p MCP_ENDPOINT=請輸入 MCP Endpoint（例如 wss://api.xiaozhi.me/mcp/?token=xxx）: 
+set /p MCP_ENDPOINT=wss://api.xiaozhi.me/mcp/?token=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQzNDQyMiwiYWdlbnRJZCI6NTUyOTU5LCJlbmRwb2ludElkIjoiYWdlbnRfNTUyOTU5IiwicHVycG9zZSI6Im1jcC1lbmRwb2ludCIsImlhdCI6MTc1NDkyMzQ0NX0._fa48iY9ysdXii2wI5BFmtRmiTkiV-ntFmBYDG1-_vgxrjeOvoN_yiJFF8lp_wnZ_g5AIaqZqOwii776JRACXg : 
 echo MCP_ENDPOINT=%MCP_ENDPOINT% > .env
 echo 已將 MCP_ENDPOINT 儲存到 .env
 pip install -r requirements.txt
